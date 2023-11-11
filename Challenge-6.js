7eca8d84544b7233f78c1ed7774eb37537828a79
lucKy <luckydhinga123@gmail.com>
2023-11-11 21:27:49 +0530

    Challenge-6

---
+++ Challenge-6.js
@@ -0,0 +1,12 @@
+function sumTriplenum(a, b) {
+    if (a == b) {
+      return 3 * (a + b);
+      } 
+     else
+     {
+      return (a + b);
+     }
+   }
+  console.log(sumTriplenum(15, 20));
+  console.log(sumTriplenum(15, 15));
+  
