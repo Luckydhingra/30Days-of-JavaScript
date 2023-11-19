b34d105f836d90537c782e8d27819017531a59fb
lucKy <luckydhinga123@gmail.com>
2023-11-19 20:21:53 +0530

    Challenge-14

---
+++ Challenge-14.js
@@ -0,0 +1,20 @@
+/*Write a JavaScript function to concatenate a given string n times (default is 1). Go to the editor
+Test Data :
+console.log(repeat('Ha!'));
+console.log(repeat('Ha!',2));
+console.log(repeat('Ha!',3));
+
+Outputs
+"Ha!"
+"Ha!Ha!"
+"Ha!Ha!Ha!"*/
+
+function repeat(str, count) {
+    if(typeof(count) == "undefined") {
+    count =1;
+  }
+  return count < 1 ? '' : new Array(count + 1).join(str);
+    }
+console.log(repeat(' 30-Days'));
+console.log(repeat(' of',2));
+console.log(repeat(' JavaScript',3));
